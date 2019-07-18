d125650a3169de0246772953e72cb6640137291c
Ricardo <richago29@gmail.com>
2019-07-18 16:56:24 -0500

    anado funciones para mejorar resposibilidad y contenido adicional

--- Javascript/funciones.js
+++ Javascript/funciones.js
@@ -1,42 +1,50 @@
 // funcion recursiva para aplicar clase global a todos los elementos
+var parfLista = document.querySelectorAll(".cmtRecm");
+var lista = document.querySelectorAll(".lista > li > input");
 
-function recurAdd(tag)
-{
+(function (event) {
+    event.preventDefault();
+    for (j = 0; j < lista.length; j++) {
+        lista[j].addEventListener("click", ocultar);
+    }
+});
+
+
+function recurAdd(tag) {
     var temp = document.querySelector(tag);
     temp.classList.add("global");
-    if(!temp.children.length>0)
-    {
+    if (!temp.children.length > 0) {
         return;
-    }
-    else
-    {
-        for(var i=0;i<temp.children.length;i++)
-        {
-            recurAdd(tag + ">" + ":nth-child("+ Number(i+1) +")");   
+    } else {
+        for (var i = 0; i < temp.children.length; i++) {
+            recurAdd(tag + ">" + ":nth-child(" + Number(i + 1) + ")");
         }
     }
     return;
 }
 
 // funcion recursiva para quitarlas
-function recurRem(tag)
-{
+function recurRem(tag) {
     var temp = document.querySelector(tag);
     temp.classList.remove("global");
-    if(!temp.children.length>0)
-    {
+    if (!temp.children.length > 0) {
         return;
-    }
-    else
-    {
-        for(var i=0;i<temp.children.length;i++)
-        {
-            recurRem(tag + ">" + ":nth-child("+ Number(i+1) +")");   
+    } else {
+        for (var i = 0; i < temp.children.length; i++) {
+            recurRem(tag + ">" + ":nth-child(" + Number(i + 1) + ")");
         }
     }
     return;
 }
-
+var ocultar = function () {
+    for (i = 0; i < lista.length; i++) {
+        if (lista[i].checked == false) {
+            parfLista[i].classList.add("oculto");
+        } else if (lista[i].checked == true) {
+            parfLista[i].classList.remove("oculto");
+        }
+    }
+}
 // corriendo funciones
 recurAdd("html");
 //recurRem("html");
